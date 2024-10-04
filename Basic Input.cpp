4615d524dc421ced69d3946a8f3a88546fa92227
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 09:41:39 +0530

    Add files via upload

---
+++ Basic Input.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std; 
+
+int main(){
+    string a;
+    cout<<"Enter an input: "<<endl;
+    cin>>a;
+    cout<<"Output is: "<<a;
+}
---
+++ Calculator  design.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+     float division,a,b,sum,difference,product;
+     cout<<"Enter two numbers: ";
+     cin>>a>>b;
+     sum = a+b;
+     difference = a-b;
+     product = a*b;
+     division = a/b;
+     cout<<"Sum is: "<<sum<<endl;
+     cout<<"Difference is: "<<difference<<endl;
+     cout<<"Product is: "<<product<<endl;
+     cout<<"Division is: "<<division<<endl;
+
+}
+
